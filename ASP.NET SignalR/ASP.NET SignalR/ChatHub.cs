ea4a704df0b5110ec0a4a9c62d1e5bd8bd28c916
eremushk <eremeevlasha@yandex.ru>
2024-05-24 17:02:16 +0300

    initialyze

---
+++ ASP.NET SignalR/ASP.NET SignalR/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ASP.NET_SignalR
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await this.Clients.All.SendAsync("Receive", message, userName);
+        }
+    }
+}
---
+++ ASP.NET SignalR/ASP.NET SignalR/Program.cs
@@ -0,0 +1,36 @@
+using ASP.NET_SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();      // подключема сервисы SignalR
+
+// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");   // ChatHub будет обрабатывать запросы по пути /chat
+
+// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
